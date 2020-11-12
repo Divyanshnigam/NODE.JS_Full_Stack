4752d1e8fde689e8b504ab02ec155e0ab5336081
Divyanshnigam <divyanshnigam1612@gmail.com>
2020-11-13 02:11:45 +0530

    commited

---
+++ JavaScript/JavaScript-1/Array_Add_Remove.js
@@ -0,0 +1,13 @@
+let fruits=["apple","mango"]
+//push and pop
+fruits.push("melon","cherry");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+
+//shift and unshift
+fruits.shift();
+console.log(fruits);
+
+fruits.unshift("kiwi","grapes");
+console.log(fruits);
