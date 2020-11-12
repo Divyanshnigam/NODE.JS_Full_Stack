1198f312fa50280c41d8802c94bef0ab66b5c782
Divyanshnigam <divyanshnigam1612@gmail.com>
2020-11-12 12:53:34 +0530

    JS

---
+++ JavaScript/JavaScript-1/ArrayObjects_Methods.js
@@ -0,0 +1,24 @@
+//ARRAY
+
+let arr=["Apple","Mango","Guava"];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr.length);
+arr.push("Banana");
+for(let i=0;i<5;i++)
+{
+    console.log(arr[i]);
+}
+console.log(arr.pop());
+for(let i=0;i<5;i++)
+{
+    console.log(arr[i]);
+}
+console.log(arr.indexOf("Mango"));
+
+console.log(arr.shift()); // remove from front
+console.log(arr.unshift("Kiwi"));  // add at first
+for(let i=0;i<5;i++)
+{
+    console.log(arr[i]);
+}
--- JavaScript/JavaScript-1/JS_Basics.js
+++ JavaScript/JavaScript-1/JS_Basics.js
@@ -1,1 +1,40 @@
-var
+let a=10;
+console.log(a);
+
+let b=[1,2,3,4,5];
+console.log(b);
+console.log("Hello Word!!");
+
+c=20;   // Global Variable
+var d=30;  //Function Scope
+let e=50;   // Block Scope
+
+function fun()
+{
+    let a=5;
+    if(a==5)
+    {
+        let b=10;
+        console.log("Inside",b);
+    }
+    console.log("Outside",b);
+}
+fun();
+console.log("Global",c);  //accesible since Globally declared
+
+function square_root(n)   // function defined in this manner are moved to top acc. to script
+{                                // rest are not becox of function hoisting
+    console.log("In 1st funt ",Math.sqrt(n));
+    return ;
+}       // if the call made above fn declation its executable
+
+square_root(10);
+
+//FUNCTION HOISTING
+var sqrt_n = function square_root(n)
+{  // this and the rest of fn in this manners are not executed if called above it.
+    console.log("In 2nd funt sqrt_n");
+    return ;
+}
+square_root(10);
+sqrt_n(10);
