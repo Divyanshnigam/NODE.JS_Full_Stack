59d8b8d59135aa674da4d8b6f6cd2b4fc5bb2533
Divyanshnigam <divyanshnigam1612@gmail.com>
2020-11-13 18:11:22 +0530

    JS

--- JavaScript/JavaScript-2/JS-Function_Closure_Protptypes/String_literals.js
+++ JavaScript/JavaScript-2/JS-Function_Closure_Protptypes/String_literals.js
@@ -2,4 +2,30 @@
 
 let s1 = 'He said, "This is awesome"'
 
-let s1 = "He said, \"This is awesome\""
+let s2 = "He said, \"This is awesome\""
+console.log(s1)
+console.log(s2)
+
+let s3 = "He said, 'This is awesome'"
+console.log(s3)
+
+let s4=`He said, 'This is awesome`
+console.log(s4)
+
+let s5 = "He said,\n 'This is awesome'"
+console.log(s5)
+
+
+let s6 = `
+He said
+ 'This is awesome'
+`
+ console.log(s6)
+
+ let name ='Divyansh'
+ let g1='Good Morning ' + name
+ console.log(g1)
+ let g2=`Good Morning ${name}`
+console.log(g2)
+ let s7=`10+2 = ${10+2}`
+ console.log(s7)
