03b9cae5e72ab65a39b6d68460efcb5b1a8e9c12
Divyanshnigam <divyanshnigam1612@gmail.com>
2020-11-18 16:16:39 +0530

    js

---
+++ JavaScript/JavaScript-3/Function_Call_Stack.js
@@ -0,0 +1,27 @@
+function ceo()
+{
+    console.log("Let's make a dent in the universe ");
+    cto()
+}
+function cto()
+{
+    console.log("Let's make a gamechanging product ");
+    vpenngg();
+}
+function vpenngg()
+{
+    console.log("Let's make a new JavaScript framework")
+    techlead()
+
+}
+function techlead()
+{
+    console.log("Lets's port Angular to Typescript")
+    developer()
+}
+function developer()
+{
+    console.log("Let's copy some code from Stackoverflow");
+    throw  new Error("The code did'nt work");
+}
+ceo()
