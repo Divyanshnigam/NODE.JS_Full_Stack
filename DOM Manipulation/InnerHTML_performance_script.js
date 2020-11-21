b8991b89d9f9edfdc8e053d769683d859405aca6
Divyanshnigam <divyanshnigam1612@gmail.com>
2020-11-21 13:06:21 +0530

    JS

--- DOM Manipulation/InnerHTML_performance_script.js
+++ DOM Manipulation/InnerHTML_performance_script.js
@@ -6,10 +6,19 @@ window.onload = function(){
     print.onclick = function(){
         let start =new Date().getTime()
         let N=parseInt(num.value)
+        let listHTML=''
         for(let i=1;i<=N;i++)
         {
-            list.innerHTML += '<li>' + i + '</li>'
+            listHTML+= '<li>' + i + '</li>'
         }
+        list.innerHTML =listHTML
         console.log(new Date().getTime()-start);
     }
 }
+
+/*
+        for(let i=1;i<=N;i++)   //...or like this but will take a lot time..
+        {
+            list.innerHTML += '<li>' + i + '</li>'
+        }
+*/
