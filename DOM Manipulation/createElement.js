d411510284152ea0192fa434a962778023400c86
Divyanshnigam <divyanshnigam1612@gmail.com>
2020-11-21 13:40:19 +0530

    DOM

--- DOM Manipulation/createElement.js
+++ DOM Manipulation/createElement.js
@@ -6,12 +6,14 @@ window.onload = function(){
     print.onclick = function(){
         let start =new Date().getTime()
         let N=parseInt(num.value)
-        let listHTML=''
+
         for(let i=1;i<=N;i++)
         {
-            listHTML+= '<li>' + i + '</li>'
+            let item = document.createElement('li')
+            item.innerText=i
+            list.appendChild(item)
+
         }
-        list.innerHTML =listHTML
         
         console.log(new Date().getTime()-start);
     }
